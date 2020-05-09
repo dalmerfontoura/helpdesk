4bff2cfbdc32cf0d030310d9c7844e2cfbe5719d
Dalmer Fontoura <dalmerfontoura@gmail.com>
2020-05-09 14:50:38 -0300

    nova vesao node

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Helpdesk
+# HelloWord
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.5.
 
 ## Development server
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to helpdesk!');
+    expect(page.getTitleText()).toEqual('helloWord app is running!');
   });
 
   afterEach(async () => {
