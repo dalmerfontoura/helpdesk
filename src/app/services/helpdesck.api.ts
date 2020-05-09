a94735b6cf26c28eac3aebe1918f8a386d0f5c8f
Dalmer Fontoura <dalmerfontoura@gmail.com>
2020-05-09 15:53:43 -0300

    service user

---
+++ src/app/services/helpdesck.api.ts
@@ -0,0 +1,1 @@
+export const HELPDESK_API = 'http://localhost:8080';
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/user.model';
 import { Injectable } from '@angular/core';
+import { HELPDESK_API } from './helpdesck.api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user: User) {
+    return this.http.post(`${HELPDESK_API}/api/auth`, user);
+  }
+
+  createOrUpdate(user: User) {
+    if (user.id != null && user.id != '') {
+      return this.http.put(`${HELPDESK_API}/api/user`, user);
+    } else {
+      this.http.post(`${HELPDESK_API}/api/user`, user);
+    }
+  }
+
+  findAll(page: number, count: number) {
+    return this.http.get(`${HELPDESK_API}/api/user/${page}/${count}`);
+  }
+
+  findById(id: string) {
+    return this.http.get(`${HELPDESK_API}/api/user/${id}`);
+  }
+
+  delete(id: string) {
+    return this.http.delete(`${HELPDESK_API}/api/user/${id}`);
+  }
 }
